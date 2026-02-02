c80dd65ab6addacc419a8e140b2b19ddb2dde597
Lucas Edson <lucasedson654@gmail.com>
2026-02-02 12:03:21 -0300

    feat: Interface de Inicialização e Execução da api no ambiente local

--- dev.py
+++ dev.py
@@ -1,11 +1,13 @@
-# dev.py
 import os
-from functions_framework._cli import _cli
+import sys
+from functions_framework import create_app
 
-def run_dev():
-    os.environ["FUNCTION_TARGET"] = "hello_star_wars"
-    os.environ["FUNCTION_SOURCE"] = "src/interfaces/handlers.py"
-    _cli()
+# Adiciona o diretório atual ao path para evitar erros de importação local
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
-    run_dev()
+    print("Iniciando ambiente de desenvolvimento")
+
+    app = create_app(target="star_wars_insights", source="main.py")
+
+    app.run(host="0.0.0.0", port=8080, threaded=False)
