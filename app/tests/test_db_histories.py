997c908a469d3a2bb40bd910b870c284d8321533
Lucas Edson <lucasedson654@gmail.com>
2026-02-03 13:23:07 -0300

    test: testa metodos do histórico das consultas do usuario

---
+++ app/tests/test_db_histories.py
@@ -0,0 +1,34 @@
+import pytest
+from app.models.database import FirestoreManager
+import os
+import dotenv
+
+dotenv.load_dotenv()
+project_id = os.getenv("GCP_PROJECT_ID")
+db_manager = FirestoreManager(project_id)
+
+user = "test@test.com"
+
+def test_create_history():
+    
+    db_manager.create_or_update_my_search_history(user, "Altura do Yoda")
+    history = db_manager.get_my_search_history(user)
+
+
+    assert "queries" in history
+    assert any(q["query"] == "Altura do Yoda" for q in history["queries"])
+
+
+def test_update_history():
+    db_manager.create_or_update_my_search_history(user, "Altura do Yoda")
+    db_manager.create_or_update_my_search_history(user, "Altura do Darth Vader")
+    history = db_manager.get_my_search_history(user)
+
+    assert "queries" in history
+    assert any(q["query"] == "Altura do Darth Vader" for q in history["queries"])
+
+
+def test_delete_history():
+    db_manager.delete_history(user)
+    history = db_manager.get_my_search_history(user)
+    assert history is None
