a8a96113ab54128a21f99461f138b7d1b075743d
Lucas Edson <lucasedson654@gmail.com>
2026-02-04 09:34:55 -0300

    fiz: Ajusta configura~çoes de CORS para produção

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ from flask import Response
 import json
 import base64
 
-# Configuração de Ambiente
+
 base_path = Path(__file__).resolve().parent
 load_dotenv(dotenv_path=base_path / ".env")
 
-# Injeção de dependência
+
 db_manager = FirestoreManager(os.getenv("GCP_PROJECT_ID"))
 swapi_client = SWAPIClient()
 controller = InsightController(db_manager, swapi_client)
 
 @functions_framework.http
 def star_wars_insights(request):
-    # --- 1. TRATAMENTO DE CORS (Obrigatório para Frontend) ---
     if request.method == "OPTIONS":
         headers = {
             "Access-Control-Allow-Origin": "https://lucasedson.github.io",
@@ -31,30 +30,25 @@ def star_wars_insights(request):
         }
         return ("", 204, headers)
 
-    # Headers de resposta padrão para todas as outras requisições
     response_headers = {"Access-Control-Allow-Origin": "https://lucasedson.github.io"}
 
-    # 2. IDENTIFICAÇÃO DO USUÁRIO
     auth_header = request.headers.get("Authorization")
     user_data = None
     user_info_encoded = request.headers.get("X-Apigateway-Api-Userinfo")
     
     if user_info_encoded:
-        # Se o Gateway mandou esse header, ele é a autoridade máxima
         decoded = base64.b64decode(user_info_encoded + "===").decode("utf-8")
         user_data = json.loads(decoded)
     else:
-        # Fallback para local ou chamadas sem Gateway
         auth_header = request.headers.get("Authorization")
         if auth_header and auth_header.startswith("Bearer "):
             user_data = verify_google_token(auth_header.split(" ")[1])
     if auth_header and auth_header.startswith("Bearer "):
         user_data = verify_google_token(auth_header.split(" ")[1])
 
-    # 3. ROTEAMENTO
     path = request.path.strip("/")
 
-    # Função auxiliar para injetar headers em respostas do controller
+
     def wrap_cors(controller_response):
         # Use o seu domínio específico em vez de "*"
         response_headers = {
@@ -84,7 +78,7 @@ def star_wars_insights(request):
         return controller_response, 200, response_headers
  
     if path == "login":
-        return wrap_cors(controller.handle_login()) # Adicionado wrap_cors aqui
+        return wrap_cors(controller.handle_login())
     
     if path == "callback":
         return wrap_cors(controller.handle_callback(request))
@@ -98,5 +92,4 @@ def star_wars_insights(request):
     if path == "history":
         return wrap_cors(controller.get_my_history(user_data))
     
-    # Busca principal (Root /)
-    return wrap_cors(controller.handle_request(request, user_data=user_data))
+    return wrap_cors(controller.handle_insight(request, user_data=user_data))
